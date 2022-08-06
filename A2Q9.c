c077467844fae3bd39be385f35ab7a1cda2a7161
pankajsingh7275 <110216514+pankajsingh7275@users.noreply.github.com>
2022-08-06 12:40:07 +0530

    Add files via upload

---
+++ A2Q9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("size of int is %d byte\n",sizeof(int));
+    printf("size of char is %d byte\n",sizeof(char));
+    printf("size of float is %d byte\n",sizeof(float));
+    printf("size of double is %d byte\n",sizeof(double));
+    getch();
+
+
+}
