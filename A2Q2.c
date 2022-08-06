6dc2d734c0d87addd45f1ba3eb16d77c5a7e8143
pankajsingh7275 <110216514+pankajsingh7275@users.noreply.github.com>
2022-08-06 12:37:52 +0530

    Add files via upload

---
+++ A2Q2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+     int a,b;
+     printf("enter a number to print without its last digit");
+     scanf("%d",&a);
+     b=a/10;
+     printf("the no is %d",b);
+     return 0;
+   
+}
