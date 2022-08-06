1cb302b8596c33c766faf5199a3e32d795ff2d10
pankajsingh7275 <110216514+pankajsingh7275@users.noreply.github.com>
2022-08-06 12:41:03 +0530

    Add files via upload

---
+++ A2Q11.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter a number and a digit to make a different number ");
+    scanf("%d %d",&a,&b);
+    c=a*10+b;
+    printf("the resulting nomis %d",c);
+    getch();
+
+
+
+}
