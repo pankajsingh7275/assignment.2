a47454a59d26e4e4bed147de325259167678822b
pankajsingh7275 <110216514+pankajsingh7275@users.noreply.github.com>
2022-08-06 12:39:17 +0530

    Add files via upload

---
+++ A2Q6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+     char c;
+     printf("enter a character to know its ASCII code");
+     scanf("%c",&c);
+     printf("ASCII code is %d",c);
+     return 0;
+
+
+
+
+}
