7916b5b36a3c067ba2c4ca4473171a7ca2f6365e
pankajsingh7275 <110216514+pankajsingh7275@users.noreply.github.com>
2022-08-06 12:40:29 +0530

    Add files via upload

---
+++ A2Q10.c
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,num;
+    printf("enter a number");
+    scanf("%d",&n);
+    num=n/10;
+    n=num*10;
+    printf("the no is %d",n);
+    getch();
+
+
+
+}
