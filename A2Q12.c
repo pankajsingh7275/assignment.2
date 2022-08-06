76a162b7431acdaa5e73f0862119e9e7257ea840
pankajsingh7275 <110216514+pankajsingh7275@users.noreply.github.com>
2022-08-06 12:41:25 +0530

    Add files via upload

---
+++ A2Q12.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float inr,usd;
+    printf("enter amount in INR to convert in to USD");
+    scanf("%f",&inr);
+    usd=inr/76.23;
+    printf("the amount is %f USD ",usd);
+    getch();
+
+
+
+}
