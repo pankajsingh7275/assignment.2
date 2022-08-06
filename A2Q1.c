625b0e9db098eaadbd9592bf7b3d68d29563c2cb
pankajsingh7275 <110216514+pankajsingh7275@users.noreply.github.com>
2022-08-06 12:35:29 +0530

    Add files via upload

---
+++ A2Q1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+     int n,ud;
+     printf("enter a number to know its unit digit");
+     scanf("%d",&n);
+     ud=n%10;
+     printf("the unit digit of %d is %d",n,ud);
+     return 0;
+
+
+
+
+
+}
